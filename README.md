642cffef6669b1e7dbe930e526fa07f1364bf1f8
root <root@DESKTOP-SSD.localdomain>
2020-03-30 00:41:09 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Put this into ./code/DockerModules
---
+++ SingleAddressPageCheckout/Block/Checkout/LayoutProcessor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DockerModules\SingleAddressPageCheckout\Block\Checkout;
+
+class LayoutProcessor
+{
+
+    /**
+     * @param \Magento\Checkout\Block\Checkout\LayoutProcessor $subject
+     * @param array $jsLayout
+     * @return array
+     */
+
+    public function afterProcess(
+        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
+        array $jsLayout
+    )
+    {
+        // get billing address form at billing step
+        $billingAddressForm = $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['afterMethods']['children']['billing-address-form'];
+
+        // move address form to shipping step
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['billing-address-form'] = $billingAddressForm;
+
+        // remove form from billing step
+        unset($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['afterMethods']['children']['billing-address-form']);
+
+        return $jsLayout;
+    }
+}
